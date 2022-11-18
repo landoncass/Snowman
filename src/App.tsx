693e42c1ad53402a4a0e5b8dee133ffda7e98814
Landon Cassman <Landon.Cassman@gmail.com>
2022-11-17 20:57:25 -0500

    Added state and game class

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
-  return <div>Hello, World</div>
+  const [game, setGame] = useState({
+    board: ['_', 'O', '_', '_', '_', '_'],
+  })
+
+  return (
+    <div>
+      <main>
+        <h1>Do you want to build a snowman?</h1>
+        <img src="/snowman/step_7.png" height="300px"></img>
+        <ul>
+          <li>{game.board[0]}</li>
+          <li>{game.board[1]}</li>
+          <li>{game.board[2]}</li>
+          <li>{game.board[3]}</li>
+          <li>{game.board[4]}</li>
+          <li>{game.board[5]}</li>
+        </ul>
+        <div className="letters">
+          <button>A</button>
+          <button>B</button>
+          <button>C</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+          <button>A</button>
+        </div>
+      </main>
+    </div>
+  )
 }
