6fc3436a83f49a63788904907ced88f3d5535a22
Landon Cassman <Landon.Cassman@gmail.com>
2022-11-28 20:58:34 -0500

    Trying to useEffect for alphabet array

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import wordList from '../wordList.json'
 
+const randomWord =
+  wordList[Math.floor(Math.random() * wordList.length)].toUpperCase()
+
 export function App() {
-  const randomWord =
-    wordList[Math.floor(Math.random() * wordList.length)].toUpperCase()
   console.log(randomWord.toUpperCase())
-  const alphabetArray = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-  ]
+
+  type alphabetLetterType = {
+    text: string
+    clicked: boolean
+  }
+
+  useEffect(function () {
+    console.log('this runs when component first runs')
+    setAlphabetArray(alphabetArray)
+  }, [])
+
+  const [alphabetArray, setAlphabetArray] = useState([
+    { text: 'A', clicked: false },
+    { text: 'B', clicked: false },
+    { text: 'C', clicked: false },
+    { text: 'D', clicked: false },
+    { text: 'E', clicked: false },
+    { text: 'F', clicked: false },
+    { text: 'G', clicked: false },
+    { text: 'H', clicked: false },
+    { text: 'I', clicked: false },
+    { text: 'J', clicked: false },
+    { text: 'K', clicked: false },
+    { text: 'L', clicked: false },
+    { text: 'M', clicked: false },
+    { text: 'N', clicked: false },
+    { text: 'O', clicked: false },
+    { text: 'P', clicked: false },
+    { text: 'Q', clicked: false },
+    { text: 'R', clicked: false },
+    { text: 'S', clicked: false },
+    { text: 'T', clicked: false },
+    { text: 'U', clicked: false },
+    { text: 'V', clicked: false },
+    { text: 'W', clicked: false },
+    { text: 'X', clicked: false },
+    { text: 'Y', clicked: false },
+    { text: 'Z', clicked: false },
+  ])
 
   const [word, setWord] = useState(randomWord)
   const [correctGuesses, setCorrectGuesses] = useState<string[]>([])
-  console.log(word)
+  // console.log(word)
 
   const wordArray = Array.from(word)
 
@@ -51,6 +63,7 @@ export function App() {
     <div>
       <main>
         <h1>Do you want to build a snowman?</h1>
+        <p></p>
         <img src="/snowman/step_7.png" height="300px"></img>
         <ul>
           {wordArray.map((char, i) => {
@@ -59,8 +72,12 @@ export function App() {
         </ul>
         <div className="letters">
           {alphabetArray.map((alphabet, index) => (
-            <button key={index} onClick={() => handleLetterClick(alphabet)}>
-              {alphabet}
+            <button
+              key={index}
+              onClick={() => handleLetterClick(alphabet.text)}
+              className={alphabet.clicked ? 'clicked' : ''}
+            >
+              {alphabet.text}
             </button>
           ))}
         </div>
